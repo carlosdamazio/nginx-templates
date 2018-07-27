9d68838f34721136edfb25e89b612bb51555cb5f
Carlos Damázio <kaduardobsb95@gmail.com>
2018-07-27 16:05:40 -0300

    Initialized parser.

--- nginx_generator/nginx_template_generator.py
+++ nginx_generator/nginx_template_generator.py
@@ -1,3 +1,6 @@
+from nginx_generator.parser import parser
+
+
 def run():
-
-
+    print("*************Executing parser!***************")
+    args = parser.parse_args()
---
+++ nginx_generator/parser/parser.py
@@ -0,0 +1,19 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Generate Nginx *.conf templates"
+    )
+    parser.add_argument('-t', dest='type',
+        help='Type of Nginx configuration'
+    )
+    parser.add_argument('-p', dest='path', default='./')
+    parser.add_argument('--server-name', dest='server',
+        default='default-server'
+    )
+    parser.add_argument('--nodes', dest='nodes', nargs='+')
+
+    args = parser.parse_args()
+
+    return args
